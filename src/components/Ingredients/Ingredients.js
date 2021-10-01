fc284528c4e7fd5be5c288557d058a7e2c303e88
Anna Ossowska <ossowska.anna1@gmail.com>
2021-10-01 21:02:44 +0200

    Managing the state with useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useReducer } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -18,15 +18,30 @@ const ingredientReducer = (state, action) => {
   }
 };
 
+const httpReducer = (state, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { isLoading: true, error: null };
+    case 'RESPONSE':
+      return { ...state, isLoading: false };
+    case 'ERROR':
+      return { isLoading: false, error: action.errorMsg };
+    case 'CLEAR':
+      return { ...state, error: null };
+    default:
+      throw new Error('Should not get there');
+  }
+};
+
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-
-  // const [userIngredients, setUserIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+    isLoading: false,
+    error: null,
+  });
 
   const addIngredientHandler = (ing) => {
-    setIsLoading(true);
+    dispatchHttp({ type: 'SEND' });
     fetch(
       'https://react-http-92c39-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json',
       {
@@ -37,24 +52,22 @@ const Ingredients = () => {
     )
       .then((response) => {
         if (response.ok) {
-          setIsLoading(false);
+          dispatchHttp({ type: 'RESPONSE' });
           return response.json();
         }
       })
       .then((data) => {
         // .name comes from the FireBase
-        // setUserIngredients((prev) => [...prev, { id: data.name, ...ing }]);
         dispatch({ type: 'ADD', ingredient: { id: data.name, ...ing } });
       });
   };
 
   const loadIngredientsHandler = useCallback((filteredIngredients) => {
     dispatch({ type: 'SET', ingredients: filteredIngredients });
-    // setUserIngredients(filteredIngredients);
   }, []);
 
   const removeItemHandler = (ingredientId) => {
-    setIsLoading(true);
+    dispatchHttp({ type: 'SEND' });
     fetch(
       `https://react-http-92c39-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`,
       {
@@ -63,29 +76,30 @@ const Ingredients = () => {
     )
       .then((response) => {
         if (response.ok) {
-          setIsLoading(false);
-          // setUserIngredients((prev) => prev.filter((ing) => ing.id !== id));
           dispatch({ type: 'DELETE', id: ingredientId });
+          dispatchHttp({ type: 'RESPONSE' });
         }
       })
       .catch((err) => {
-        // These two lines run synchronously
-        // But React batches two states together and produces ONE render cycle
-        setError(`Something went wrong. ${err.message}.`);
-        setIsLoading(false);
+        dispatchHttp({
+          type: 'ERROR',
+          errorMsg: `Something went wrong. ${err.message}`,
+        });
       });
   };
 
   const clearError = () => {
-    setError(null);
+    dispatchHttp({ type: 'CLEAR' });
   };
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {httpState.error && (
+        <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>
+      )}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
-        loading={isLoading}
+        loading={httpState.isLoading}
       />
 
       <section>
